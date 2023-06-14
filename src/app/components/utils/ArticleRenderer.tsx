44f63da3f79f28f06f2812b56b22bb6891256585
Ishnoor Singh <ishnoors@gmail.com>
2023-06-14 10:57:43 -0700

    proof of concept of loading a article as md

---
+++ src/app/components/utils/ArticleRenderer.tsx
@@ -0,0 +1,30 @@
+'use client'
+import * as React from 'react';
+import { getArticleContentFromURL } from '@/app/functions/getArticleContent'; 
+import ReactMarkdown from 'react-markdown';
+
+type ArticleRendererProps = {
+    url: string;
+}
+
+export default function ArticleRenderer({ url }: ArticleRendererProps) {
+    console.log(url)
+    const [md, setMd] = React.useState('');
+    React.useEffect(() => {
+        async function fetchMd() {
+            await getArticleContentFromURL(url)
+            .then((md) => {
+                setMd(md);
+            }).catch((err) => {
+                console.error(err);
+                setMd('# Something went wrong');
+            })
+        }
+        fetchMd();
+    }, []);
+    return (
+        <>
+            {md ? <ReactMarkdown>{ md }</ReactMarkdown> : <p>Loading...</p>}
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,12 @@
 'use client'
 import * as React from 'react';
-import { getArticleContentFromURL } from './functions/getArticleContent'; 
+import ArticleRenderer from './components/utils/ArticleRenderer';
+
 export default function Home() {
-    const [html, setHtml] = React.useState('');
-    React.useEffect(() => {
-        async function fetchHtml() {
-            await getArticleContentFromURL('https://gamerant.com/best-open-world-games-switch-brain-off-while-playing/')
-            .then((html) => {
-                console.log('html', html)
-                setHtml(html);
-            }).catch((err) => {
-                console.error(err);
-                setHtml('<h1>Something went wrong</h1>');
-            })
-        }
-        fetchHtml();
-    }, []);
     return (
         <>
             <h1>Article Chatbot</h1>
-            <div dangerouslySetInnerHTML={{__html: html}} />
+            <ArticleRenderer url = 'https://techcrunch.com/2023/06/07/blush-ai-dating-sim-replika-sexbot/'/>
         </>
     )
 }
