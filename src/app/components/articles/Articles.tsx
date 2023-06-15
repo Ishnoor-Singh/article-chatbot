2d6b618aee208e490c2e32f7c5265892d374027e
Ishnoor Singh <ishnoors@gmail.com>
2023-06-14 21:34:05 -0700

    wip

--- src/app/components/articles/Articles.tsx
+++ src/app/components/articles/Articles.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import AddArticle from "./AddArticle";
-
+import dotenv from 'dotenv';
 
 export default async function Articles() {
+//     const articles: any[] = await getAllArticles();
     const articles: any[] = await getArticleList();
+
     return (
         <div className="page-container">
             <div className="half-page-container article-links-container">
@@ -18,12 +20,14 @@ export default async function Articles() {
 }
 
 const getArticleList = async () => {
-    const res = await fetch('http://127.0.0.1:8090/api/collections/articles/records?page=1&perPage=30', { cache: 'no-store' });
+    dotenv.config();
+
+    const res = await fetch(`http://127.0.0.1:8090/api/collections/articles/records?page=1&perPage=30`, { cache: 'no-store' });
     const data = await res.json();
     console.log(data);
     return data?.items as any[];
-    // dotenv.config();
-    // return await (new PocketBase(process.env.POCKETBASE_URL)).collection('articles').getList(1, 100).then((records) => records.items)
+    // // return await (new PocketBase(process.env.POCKETBASE_URL)).collection('articles').getList(1, 100).then((records) => records.items)
+    // return await getAllArticles()
 }
 
 // async function ArticleList() {
