bae4e61af35c929661ffa126abd741586fb71baf
Ishnoor Singh <ishnoors@gmail.com>
2023-06-14 21:26:30 -0700

    router refresh

--- src/app/components/articles/Articles.tsx
+++ src/app/components/articles/Articles.tsx
@@ -2,24 +2,47 @@ import Link from "next/link";
 import AddArticle from "./AddArticle";
 
 
-export default function Articles({articles}: ArticlesProps) {
+export default async function Articles() {
+    const articles: any[] = await getArticleList();
     return (
         <div className="page-container">
-            <AddArticle/>
             <div className="half-page-container article-links-container">
-                {articles && articles.map(article => <Article key={article.id} {...article}/>)}
+                {articles?.map(article =>  
+                <Link key={article.id} href={`/chatWithArticle/${article.id}`}>
+                    <div className="article-link">{article.title}</div>  
+                </Link>)}
             </div>
+            <AddArticle/>
         </div>
     )
 }
 
-function Article({ id, title }: Article) {
-    return (
-            <Link href={`/chatWithArticle/${id}`}>
-                <div className="article-link">{title}</div>  
-            </Link>
-    )
+const getArticleList = async () => {
+    const res = await fetch('http://127.0.0.1:8090/api/collections/articles/records?page=1&perPage=30', { cache: 'no-store' });
+    const data = await res.json();
+    console.log(data);
+    return data?.items as any[];
+    // dotenv.config();
+    // return await (new PocketBase(process.env.POCKETBASE_URL)).collection('articles').getList(1, 100).then((records) => records.items)
 }
+
+// async function ArticleList() {
+//     // const articles = await getArticleList();
+//     const articles = [];
+//     return (
+//         <div className="half-page-container article-links-container">
+//             {articles?.map(article => <Article key={article.id} title={article.title}/>)}
+//         </div>
+//     )
+// }
+
+// function Article({ id, title }: Article) {
+//     return (
+//             <Link href={`/chatWithArticle/${id}`}>
+//                 <div className="article-link">{title}</div>  
+//             </Link>
+//     )
+// }
 
 export type Article = {
     "id": string;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,34 @@
 'use client'
 import * as React from 'react';
 import Articles, { Article } from './components/articles/Articles';
-import { getAllArticles } from './functions/articleDBOperations';
 
 const articlesInit: Article[] = [];
 
-export default function Home() {
-    const [articles, setArticles] = React.useState(articlesInit);
-    React.useEffect(() => {
-        const getArticleList = async () => {
-            getAllArticles().then(res => {
-                setArticles(res || []); 
-            }).catch(err => {
-                console.log('error', err)
-            })
-        }
+// export default function Home() {
+//     const [articles, setArticles] = React.useState(articlesInit);
+//     React.useEffect(() => {
+//         const getArticleList = async () => {
+//             getAllArticles().then(res => {
+//                 setArticles(res || []); 
+//             }).catch(err => {
+//                 console.log('error', err)
+//             })
+//         }
 
-        getArticleList();
-    }, []);
+//         getArticleList();
+//     }, []);
+//     return (
+//         <div style={{width: '100vw', height: '100vh', margin: 0}}>
+//             <Articles articles={articles}/>
+//         </div>
+//     )
+// }
+
+
+export default function Home() {
     return (
         <div style={{width: '100vw', height: '100vh', margin: 0}}>
-            <Articles articles={articles}/>
+             <Articles/>
         </div>
     )
-}
-
-
+    }
